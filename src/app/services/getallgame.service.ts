334725212462e1a6dee2e4e0d2820919fd0494ad
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-08-22 11:14:31 -0500

    http getgames

--- src/app/services/getallgame.service.ts
+++ src/app/services/getallgame.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, tap, catchError }  from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -6,13 +9,34 @@ import { Injectable } from '@angular/core';
 export class GetallgameService {
 
 
-  url: string = 'https://api.rawg.io/api/games?key=';
-  key: string = '9a0493ac96bd473ea8297c8f2ad91efb';
-  page: string = '2';
-  page_size: string = '2' ;
-  count: string = '2';
+  private url: string = 'https://api.rawg.io/api/games';
+  page: number = 2;
+  page_size: number = 2 ;
+  count: number = 2;
 
 
 
-  constructor() { }
+  constructor(  private http: HttpClient  ) {  }
+
+
+    get params(){
+      return {
+        key: '9a0493ac96bd473ea8297c8f2ad91efb',
+        page: this.page.toString(),
+        page_size: this.page_size.toString(),
+        count: this.count.toString()
+      }
+    }
+
+
+    getGames (){
+
+      return this.http.get( `${this.url}`, { params: this.params } )
+      .pipe( map(  (resp )  => resp )  );
+
+    }
+
+
+
+
 }
