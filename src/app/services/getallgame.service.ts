f9f761a71e4c21d1785533d8929cb99cf47c9295
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-08-22 11:03:46 -0500

    game services

--- src/app/services/getallgame.service.ts
+++ src/app/services/getallgame.service.ts
@@ -5,5 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class GetallgameService {
 
+
+  url: string = 'https://api.rawg.io/api/games?key=';
+  key: string = '9a0493ac96bd473ea8297c8f2ad91efb';
+  page: string = '2';
+  page_size: string = '2' ;
+  count: string = '2';
+
+
+
   constructor() { }
 }
