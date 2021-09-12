d43327f63170928b255511026b5e12b4a180e357
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-09-12 10:54:13 -0500

    element afterinit

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild,  ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit, ViewChild,  ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 import {  GetallgameService  } from 'src/app/services/getallgame.service';
 import { GameList, Result } from 'src/app/interfaces/listGames';
 
@@ -12,7 +12,7 @@ export class CardComponent implements OnInit {
 
   public games:GameList = <GameList>{};
   public listGames: Result[] = [];
-
+  @ViewChild("element", { static: false }) element: ElementRef;
 
 
   constructor(  private getGamesServices: GetallgameService  ) { }
@@ -31,11 +31,7 @@ export class CardComponent implements OnInit {
       console.log(  this.listGames );
 
 
-      if( this.listGames.length > 0 ){
-
-        this.selectelement();
-
-      }
+      console.log( this.element );
 
 
 
@@ -45,6 +41,18 @@ export class CardComponent implements OnInit {
 
 
 
+  }
+
+
+  ngAfterViewInit(){
+
+      if( this.listGames.length > 0 ){
+
+        console.log( this.element );
+
+      }else{
+        console.log( 'Aun no estan cargados los elementos' );
+      }
   }
 
 
