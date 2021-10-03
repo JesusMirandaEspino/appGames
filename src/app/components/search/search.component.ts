87f3301c7b413e46136f47735aa8ddaaec8c03c7
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-10-03 10:47:12 -0500

    search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
 import { GetallgameService } from 'src/app/services/getallgame.service';
 import { GameList,Result } from 'src/app/interfaces/listGames';
 
@@ -12,6 +12,7 @@ export class SearchComponent implements OnInit {
   public games:GameList = <GameList>{};
   public listGames: Result[] = [];
 
+
   constructor( private getallgame: GetallgameService ) { }
 
   ngOnInit(): void {
@@ -20,8 +21,11 @@ export class SearchComponent implements OnInit {
 
 
 
-  gameSearch( game:string ){
+  // Funcion para buscar un un juego, recibe el juego a buscar y el elemento input para limpiarlo
+  gameSearch( game:string, input:HTMLInputElement ){
     game = game.trim();
+
+
 
     if( game.length == 0 ){
       return;
@@ -33,9 +37,23 @@ export class SearchComponent implements OnInit {
 
       this.listGames = this.games.results;
 
-      console.log( this.listGames );
+      console.log( this.listGames  );
+
+
+      // Limpiar el input
+      input.value = '';
 
     } );
+
+  }
+
+
+  selectElement(game: HTMLDivElement, cardtext:HTMLDivElement ):void {
+
+
+    game.classList.toggle('descubierto');
+
+    cardtext.classList.toggle('ocultar');
 
   }
 
